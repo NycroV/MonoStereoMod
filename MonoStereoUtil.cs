383b644bbbc53a38f2dd330626faf72cd634c077
NycroV <83246959+NycroV@users.noreply.github.com>
2024-09-01 19:54:34 -0400

    Ready for setup

---
+++ MonoStereoUtil.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System.Reflection;
+
+namespace MonoStereoMod
+{
+    public static class MonoStereoUtil
+    {
+        private static readonly MethodInfo getPath = typeof(Terraria.ModLoader.Engine.DistributionPlatform).Assembly
+            .GetType("TMLContentManager")
+            .GetMethod("GetPath", BindingFlags.Instance | BindingFlags.NonPublic, [typeof(string)]);
+
+        public static string GetPath(this ContentManager instance, string path) => (string)getPath.Invoke(instance, [path]);
+    }
+}
